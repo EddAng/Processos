6e6ee043f5b7ce5d1fdbab263a82d41e1b7b8ab0
Eduardo Angeli <eddw.angeli@gmail.com>
2025-05-13 14:25:35 -0300

    Busca_Sabores e Readme
    Adicionado nova tabela para buscar sabores e alterado Readme

---
+++ Busca_Sabores/pesquisa_sabores.py
@@ -0,0 +1,31 @@
+from openpyxl import load_workbook
+
+def receber(nome):
+    retorna = 0
+    planilha = load_workbook("Sabor_26-03-2025 09-44-06.xlsx")
+    plan = planilha.active
+    for i in range(1,plan.max_row):
+        if plan.cell(row=i,column=2).value.upper() == nome.upper():
+            id = plan.cell(row=i,column=4)
+            retorna = 1
+
+    if retorna == 0:
+        return "Item não Encontrado"
+    else:
+        return id.value
+
+desc = ""
+while desc != "sair":
+    try:
+        desc = input("Informe o nome do sabor: ")
+
+        if desc == "sair":
+            break
+
+        elif receber(desc) == "Item não Encontrado":
+            print(receber(desc))
+            
+        else:
+            print(receber(desc))
+    except:
+        print('\nAlgum erro aconteceu')
