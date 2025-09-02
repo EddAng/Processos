000aa489365fe20b5568bd74af90e15c92d77f2f
Eduardo Angeli <eddw.angeli@gmail.com>
2025-09-02 20:47:08 -0300

    Update auxilio_Integracao.py

--- Auxilio Integração Produtos/auxilio_Integracao.py
+++ Auxilio Integração Produtos/auxilio_Integracao.py
@@ -8,14 +8,14 @@ class Planilha():
         self.produto_coluna = desc
         self.id_coluna = id
         
-    def pegaCodigo(self):
+    def pegaCodigo(self,integracao):
         item = ""
         while item.upper() != "VOLTAR":
             passou = 0
             item = input(f"Informe o {self.tipo} desejado (\"Voltar\" para trocar planilha): ")
             for i in range(1,self.ativo.max_row):
                 if item.upper() == self.ativo.cell(row=i,column=2).value.upper():
-                    print(f"{item} - {ifood(self.tipo,self.ativo.cell(row=i,column=1).value)}")
+                    print(f"{item} - {codigo(integracao,self.tipo,self.ativo.cell(row=i,column=1).value)}")
                     passou = 1
             if passou == 0:
                 palavra = item.split()
@@ -25,20 +25,23 @@ class Planilha():
                         if b != "de" and "com" and "c/m" and "em":   
                             if b.upper() in self.ativo.cell(row=i,column=2).value.upper():
                                 c = c + 1
-                    if c >= 2:
-                        print(f"{self.ativo.cell(row=i,column=2).value} - {ifood(self.tipo,self.ativo.cell(row=i,column=1).value)}")       
+                    if c >= 1:
+                        print(f"{self.ativo.cell(row=i,column=2).value} - {codigo(integracao,self.tipo,self.ativo.cell(row=i,column=1).value)}")       
         
-def validarPlanilha(nome,planilhas,tipo):
+def validarPlanilha(nome,planilhas,tipo,integracao):
     try:
         if nome not in planilhas:
             planilha = load_workbook(nome+".xlsx") 
             ativo = planilha.active
-            for i in range (1,ativo.max_column):
+            for i in range (1,ativo.max_column): 
                 if ativo.cell(row=1,column=i).value.upper() == "DESCRIÇÃO":
                     descricao = i
-                elif ativo.cell(row=1,column=i).value.upper() == "ID":
+                elif ativo.cell(row=1,column=i).value.upper() == "ID" and (integracao.upper() == "ANOTA AI" and tipo != "Pizza"):
+                    id = i    
+                elif ativo.cell(row=1,column=i).value.upper() == "ID" and integracao.upper() != "ANOTA AI":
+                    id = i 
+                elif ativo.cell(row=1,column=i).value.upper() == "PRODUTO ID:" and (integracao.upper() == "ANOTA AI" and tipo == "Pizza") :
                     id = i
-            
             return Planilha(planilha,ativo,tipo,descricao,id)
         else:
             print("Planilha ja informada")
@@ -47,51 +50,65 @@ def validarPlanilha(nome,planilhas,tipo):
         print(e)
         return None
      
-def entra_Planilha(planilhas,planilha):
+def entra_Planilha(planilhas,planilha,integracao):
     tipo = None
     while not tipo:
         tipo = input(f"Informe o nome da planilha {planilha} \"Sair\" para não informar: ")
         if tipo == "Sair":
             return None
         else:
-            tipo = validarPlanilha(tipo,planilhas,planilha)
+            tipo = validarPlanilha(tipo,planilhas,planilha,integracao)
             return tipo
         
-def ifood(tipo, id):
-    if tipo == "Sabor":
-        return "S"+str(id)
-    elif tipo == "Produto":
-        return id
-    elif tipo == "Adicional":
-        return "AD"+str(id)
-    elif tipo == "Pizza":
-        return "PZ"+str(id)
+def codigo(integracao,tipo, id):
+    if integracao.upper() == "IFOOD":
+        if tipo == "Sabor":
+            return "S"+str(id)
+        elif tipo == "Produto":
+            return id
+        elif tipo == "Adicional":
+            return "AD"+str(id)
+        elif tipo == "Pizza":
+            return "PZ"+str(id)
+    elif integracao.upper() == "ANOTA AI":    
+        if tipo == "Sabor":
+            return "S"+str(id)
+        elif tipo == "Produto":
+            return id
+        elif tipo == "Adicional":
+            return id
+        elif tipo == "Pizza":
+            return id
     
+integracao = ""
+while integracao.upper() != "IFOOD" and integracao.upper() != "ANOTA AI":
+    integracao = input("Informe o tipo da integração (Ifood, Anota Ai): ")
+
 planilhas = []
 
 produto = None
-produto = entra_Planilha(planilhas,"Produto")
+produto = entra_Planilha(planilhas,"Produto",integracao=integracao)
 
 pizza = None
-pizza = entra_Planilha(planilhas,"Pizza")
+pizza = entra_Planilha(planilhas,"Pizza",integracao=integracao)
 
 adicional = None
-adicional = entra_Planilha(planilhas,"Adicional")
+adicional = entra_Planilha(planilhas,"Adicional",integracao=integracao)
 
 sabor = None
-sabor = entra_Planilha(planilhas,"Sabor")
+sabor = entra_Planilha(planilhas,"Sabor",integracao=integracao)
 
 tipo = ""
 
 while tipo != "SAIR":
     tipo = input("Informe o tipo da planilha, (\"Sair\" para finalizar): ")
     if tipo == "Produto" and produto != None:
-        produto.pegaCodigo()
+        produto.pegaCodigo(integracao)
     elif tipo == "Pizza" and pizza != None:
-        pizza.pegaCodigo()
+        pizza.pegaCodigo(integracao)
     elif tipo == "Sabor" and sabor != None:
-        sabor.pegaCodigo()
+        sabor.pegaCodigo(integracao)
     elif tipo == "Adicional" and adicional != None:
-        adicional.pegaCodigo()
+        adicional.pegaCodigo(integracao)
     elif tipo == "Sair":
         break
